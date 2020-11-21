8d12db04e0b7f5f7782f3cc0d0064f730636dbc2
dmpe <cincenko@outlook.com>
2020-11-21 14:58:14 +0100

    use ngtv

---
+++ NerbankGitVersion/Controllers/GitVersionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NerbankGitVersion.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NerbankGitVersionController : ControllerBase
+    {
+
+        [HttpGet]
+        public String GetInfo()
+        {
+            var versionNumber = typeof(NerbankGitVersionController).Assembly.FullName;
+            return versionNumber;
+        }
+    }
+}
